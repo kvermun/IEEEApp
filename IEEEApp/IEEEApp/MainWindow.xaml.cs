ca30eb376ea8ddc0cf7c9945a3a0fe12c6e7eb6d
kvermun <kartikvermun@gmail.com>
2013-12-16 21:22:50 +0530

    Working Prototype

--- IEEEApp/IEEEApp/MainWindow.xaml.cs
+++ IEEEApp/IEEEApp/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
 
 namespace IEEEApp
 {
@@ -22,6 +23,64 @@ namespace IEEEApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KinectSensor old = (KinectSensor)e.OldValue;
+            StopKinect(old);
+            KinectSensor sensor = (KinectSensor)e.NewValue;
+            sensor.DepthStream.Enable();
+            sensor.ColorStream.Enable();
+            sensor.SkeletonStream.Enable();
+            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
+            try
+            {
+                sensor.Start();
+            }
+            catch
+            {
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+
+        }
+
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    //stop sensor 
+                    sensor.Stop();
+
+                    //stop audio if not null
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+
+
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            kinectSensorChooser1.Kinect.DepthStream.Disable();
+            kinectSensorChooser1.Kinect.ColorStream.Disable();
+            kinectSensorChooser1.Kinect.SkeletonStream.Disable();
+            StopKinect(kinectSensorChooser1.Kinect);
         }
     }
 }
